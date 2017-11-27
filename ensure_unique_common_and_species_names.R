1ee6a31015eaaf70e3e217aa196da2051d3e3265
rpecchia <rpecchia@gmail.com>
2017-11-27 15:16:02 -0800

    first upload of distinct common name script

--- ensure_unique_common_and_species_names.R
+++ ensure_unique_common_and_species_names.R
@@ -2,11 +2,16 @@
 
 library(dplyr)
 
-raw_data <- read.csv("~/Desktop/Impacts Systematic Review/Crystal-Ornelas_et_al_SR_v12.csv", header=TRUE)
+raw_data <- read.csv("~/Desktop/Impacts Systematic Review/Crystal-Ornelas_et_al_SR_v13.csv", header=TRUE)
 
-common_and_species <- select(raw_data, invasivespecies, latinname)
+common_names <- select(raw_data, invasivespecies)
+latin_names <- select(raw_data, latinname)
 
-distinct_combinations_of_names <- distinct(common_and_species)
 
-arrange(distinct_combinations_of_names, latinname)
+distinct_common_names <- distinct(common_names)
+distinct_common_names <- arrange(distinct_common_names, invasivespecies)
+write.csv(distinct_common_names, file = "distinct_common_names.csv")
 
+distinct_latin_names <- distinct(latin_names)
+distinct_latin_names <- arrange(distinct_latin_names, latinname)
+write.csv(distinct_latin_names, file = "distinct_latin_names.csv")
